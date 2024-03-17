633da5251036110877ac68a374d37117bb981792
Mark <n01586869@humber.ca>
2024-03-17 01:50:45 -0400

    finalize breakfast screen

--- Breakfast.js
+++ Breakfast.js
@@ -6,38 +6,41 @@ import RecipeCardList from "./RecipeCardList";
 
 const Breakfast = ({ route, navigation }) => {
 
-  const { API, API_KEY } = route.params
+  const { API, API_KEY } = route.params // get api and api_key from route params
 
-  const [currentScreen, setCurrentScreen] = useState("")
-  const [recipes, setRecipes] = useState([])
+  // set state vars
+  const [currentScreen, setCurrentScreen] = useState("") // this is used for toggling the header applied on a stack screen
+  const [recipes, setRecipes] = useState([]) // this is data received from api
+
   const Stack = createNativeStackNavigator()
 
   useEffect(() => {
-    fetch(`${API}/${API_KEY}/filter.php?c=Breakfast`)
-    .then(res => res.json())
+    fetch(`${API}/${API_KEY}/filter.php?c=Breakfast`) // get list of Breakfasts. this returns array of objects with only meal id and thumbnail
+    .then(res => res.json()) // parse json
     .then(data => {
-      const mealInfo = data.meals.map(item => {
-        const APIFull = `${API}/${API_KEY}/lookup.php?i=${item.idMeal}`;
-        return (
+      // since array of objects contains only meal id, need to do another fetch to get every meal by meal id
+      const mealInfo = data.meals.map(item => { // stores array of promises for recipes in mealInfo buffer (eventually will use this in setRecipes)
+        const APIFull = `${API}/${API_KEY}/lookup.php?i=${item.idMeal}`; // making full api string for readability
+        return ( // returns the fetch promise which will eventually be evaluated to this specific recipe
           fetch(APIFull)
           .then(res => res.json())
           .then(data => data.meals[0])
         )
       })
-      return Promise.all(mealInfo);
+      return Promise.all(mealInfo); // returns the promise which will resolve once all the promises in mealInfo are resolved
     })
-    .then(recipes => {
-      setRecipes(recipes)
+    .then(mealInfo => {
+      setRecipes(mealInfo) // once all mealInfo promises are resolved, sets recipes to mealInfo
     })
-    .catch(error => {
+    .catch(error => { // error handling
       console.error('Error fetching recipes :', error)
     });
   }, [])
 
-  const Screen = () => {
+  const BreakfastScreen = () => {
     return( 
       <>
-        {recipes[0] ?
+        {recipes[0] ? // Loading screen while waiting for fetch
         <RecipeCardList navigation={navigation} recipes={recipes} screen={"Breakfast Recipes"}/>
         :
         <View style={{alignSelf: 'center', marginTop: 30}}><Text>Loading Recipes...</Text></View>}
@@ -47,9 +50,9 @@ const Breakfast = ({ route, navigation }) => {
 
   return(
     <Stack.Navigator screenOptions={{
-      headerShown: (currentScreen === "Recipe") ? true : false
+      headerShown: (currentScreen === "Recipe") ? true : false // hides Stack header if not in a Recipe
     }}>
-      <Stack.Screen name="Breakfast Screen" component={Screen}/>
+      <Stack.Screen name="Breakfast Screen" component={BreakfastScreen}/>
       <Stack.Screen initialParams={{setCurrentScreen}} name="Breakfast Recipes" component={Recipe}/>
     </Stack.Navigator>
   )
